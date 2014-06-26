ba5e47b8b3a31cfc7f0d68f217d8a3beb17ba21c
ChanellGCTest <chanell18@hotmail.com>
2014-06-26 11:03:58 -0700

    C# console
    used system and console for update

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputReadLine;
+            
+            Console.WriteLine("Hello World!");
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine(inputReadLine);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
